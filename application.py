2037d51c5948cdfcbb154d6626dc0330a0e06f4b
juanes-c <juan.cardona@asimetrix.co>
2022-09-21 16:05:52 -0500

    initial test

---
+++ application.py
@@ -0,0 +1,18 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+application = app.server
+app.title='Dash on AWS EB!'
+########### Set up the layout
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+    html.Div(children='''
+            This is Dash running on Elastic Beanstalk.
+        '''),
+    
+])
+########### Run the app
+if __name__ == '__main__':
+    application.run(debug=False, port=8080)
